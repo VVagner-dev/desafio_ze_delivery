7cf845901ee80ca8621ff647fdd6009a21202a60
Wagner cristo <wagnerdossantos2311@gmail.com>
2026-03-06 13:26:03 -0300

    commit inicial

---
+++ src/main/java/com/wagner/desafioze/DesafiozeApplication.java
@@ -0,0 +1,13 @@
+package com.wagner.desafioze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiozeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiozeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagner/desafioze/DesafiozeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.desafioze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiozeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
